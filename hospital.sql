f90b582cba681e4b3573a2eec540496b15501520
Fabi <fabicaneyu@gmail.com>
2021-08-29 22:09:36 -0300

    query em SQL

---
+++ hospital.sql
@@ -0,0 +1,4 @@
+INSERT  INTO hospital (cnpj, nome, endereco)
+VALUES (555555, 'Santa Casa', 'Rua Sao Jose')
+INSERT  INTO hospital (cnpj, nome, endereco)
+VALUES (777777, 'Hospital das Clinicas', 'Rua Cerqueira Cesar')
---
+++ paciente.sql
@@ -0,0 +1,4 @@
+INSERT  INTO paciente (prontuario, nome, cpf, convenio, data_consulta, especialidade)
+VALUES (11, 'Maria', 111111, 'unimed', '2021-08-26', 'neurologia');
+INSERT  INTO paciente (prontuario, nome, cpf, convenio, data_consulta, especialidade)
+VALUES (25, 'Jose', 222222, 'amil', '2021-08-26', 'psiquiatria');
